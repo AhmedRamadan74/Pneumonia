f7aeeb66ad7ef06ad2d8852a8b3a70c2c7077dd0
Ahmed Ramdan Dawood <81949345+AhmedRamadan74@users.noreply.github.com>
2023-11-02 15:22:20 +0200

    Update demo.py

--- EDA/demo.py
+++ EDA/demo.py
@@ -9,13 +9,13 @@ import os
 from PIL import Image
 
 # read data
-death=pd.read_csv(os.path.join(os.getcwd(),"death.csv"))
-risk=pd.read_csv(os.path.join(os.getcwd(),"risk.csv"))
-lower_respiratory_diseases=pd.read_csv(os.path.join(os.getcwd(),"lower_respiratory_diseases.csv"))
-gdp=pd.read_csv(os.path.join(os.getcwd(),"gdp.csv"))
-breastfeeding=pd.read_csv(os.path.join(os.getcwd(),"breastfeeding.csv"))
-careseeking=pd.read_csv(os.path.join(os.getcwd(),"careseeking.csv"))
-vaccine=pd.read_csv(os.path.join(os.getcwd(),"vaccine.csv"))
+death=pd.read_csv(os.path.join(os.getcwd(),"EDA/death.csv"))
+risk=pd.read_csv(os.path.join(os.getcwd(),"EDA/risk.csv"))
+lower_respiratory_diseases=pd.read_csv(os.path.join(os.getcwd(),"EDA/lower_respiratory_diseases.csv"))
+gdp=pd.read_csv(os.path.join(os.getcwd(),"EDA/gdp.csv"))
+breastfeeding=pd.read_csv(os.path.join(os.getcwd(),"EDA/breastfeeding.csv"))
+careseeking=pd.read_csv(os.path.join(os.getcwd(),"EDA/careseeking.csv"))
+vaccine=pd.read_csv(os.path.join(os.getcwd(),"EDA/vaccine.csv"))
 
 #######################################################
 #layout 
@@ -24,8 +24,7 @@ st.set_page_config(page_title="Pneumonia",layout="wide")
 st.title("Exploratory Analysis of Pneumonia, By [Ahmed Ramadan](https://www.linkedin.com/in/ahmed-ramadan-18b873230/)")
 
 st.header("Some inforamtion of Pneumonia : ")
-image = Image.open("_تنزيل.jpeg")
-st.image(image, caption='Pneumonia')
+image = Image.open(os.path.join(os.getcwd(),"EDA/_تنزيل.jpeg"))st.image(image, caption='Pneumonia')
 st.markdown("Pneumonia is an infection that inflames the air sacs in one or both lungs.The air sacs may fill with fluid,causing cough with phlegm or pus, fever, chills, and difficulty breathing.")
 st.markdown("Pneumonia can range in seriousness from mild to life-threatening. It is most serious for infants and young children, people older than age 65, and people with health problems or weakened immune systems")
 
